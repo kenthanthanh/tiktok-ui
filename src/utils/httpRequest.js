301e5ffb81e33c69f39292d2efaaf6f33859f1d6
Ken Than Thanh <hieukun36@gmail.com>
2023-12-03 14:13:09 +0700

    SetUp env

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
+console.log(process.env.REACT_APP_BASE_URL)
 const httpRequest = axios.create({
-    baseURL: 'https://tiktok.fullstack.edu.vn/api/'
+    baseURL: process.env.REACT_APP_BASE_URL
 })
 export default httpRequest;
